d3cb872e15dbf0d96dc866661fc6cd7936892a35
Avery R <contact@nekodjin.org>
2023-07-03 08:45:47 -0400

    add tailwindcss, cva

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: ["./src/**/*.{html,svelte,js,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
+
